712948225f8549e383bd347e22f316d41a61c444
Lucas Parma <l.parma93@gmail.com>
2025-06-14 14:19:49 -0300

    ej1: sublista

--- katamino.pl
+++ katamino.pl
@@ -1,3 +1,9 @@
 :- use_module(piezas).
 
-% Completar ...
+% Ejercicio 1: Sublista
+% sublista(+Descartar, +Tomar, +L, -R)
+sublista(Descartar, Tomar, L, R) :-
+    append(Prefijo, Sufijo, L),
+    length(Prefijo, Descartar),
+    append(R, _, Sufijo),
+    length(R, Tomar). 
