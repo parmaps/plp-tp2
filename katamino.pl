a2d42cb9e8ce02c9eae1b5366c969b914f734334
Lucas Parma <l.parma93@gmail.com>
2025-06-14 17:51:57 -0300

    ej2: tablero

--- katamino.pl
+++ katamino.pl
@@ -7,3 +7,16 @@ sublista(Descartar, Tomar, L, R) :-
     length(Prefijo, Descartar),
     append(R, _, Sufijo),
     length(R, Tomar). 
+
+
+% Ejercicio 2: Tablero
+
+% crear_fila(+K, -Fila)
+crear_fila(K, Fila) :-    
+    length(Fila, K). 
+
+% tablero(+K, -T) 
+tablero(K, T) :-
+    K > 0,
+    length(T, 5),               
+    maplist(crear_fila(K), T).
